3440a8166942c2bcc7311e180573147f4a187ad2
Tomi Lämsä <lamsatom@gmail.com>
2019-08-10 17:09:22 +0300

    unicafe step 5 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from 'react-dom'
 
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = (props) => {
 
   const calcTotal = () => {
-    return good + neutral + bad;
+    return props.votes.good + props.votes.neutral + props.votes.bad;
   }
 
   const calcAverage = () => {
     const total = calcTotal();
     if (total > 0) {
-      const badScore = bad * -1;
-      const goodScore = good;
+      const badScore = props.votes.bad * -1;
+      const goodScore = props.votes.good;
       // neutral votes are worth 0
       return (goodScore + badScore) / total;
     }
@@ -27,10 +23,34 @@ const App = () => {
   const calcPositivePercentage = () => {
     const total = calcTotal();
     if (total > 0) {
-      return `${good / total * 100}%`;
+      return `${props.votes.good / total * 100}%`;
     }
     return 'No votes registered';
   }
+
+  if (calcTotal() > 0){
+    return (
+      <>
+        <h2>Results</h2>
+        <StatsRow text="Good:" value={props.votes.good}></StatsRow>
+        <StatsRow text="Neutral:" value={props.votes.neutral}></StatsRow>
+        <StatsRow text="Bad:" value={props.votes.bad}></StatsRow>
+        <br></br>
+        <StatsRow text="Votes total:" value={calcTotal()}></StatsRow>
+        <StatsRow text="Average score:" value={calcAverage()}></StatsRow>
+        <StatsRow text="Amount positive:" value={calcPositivePercentage()}></StatsRow>
+      </>
+    )
+  }
+  return null; 
+};
+
+const StatsRow = (props) => <p>{props.text} {props.value}</p>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   const onClickFactory = (type) => {
     if (type === 'good') return () => setGood(good + 1);
@@ -47,14 +67,8 @@ const App = () => {
       <Button onClick={onClickFactory('neutral')} text="neutral"></Button>
       <Button onClick={onClickFactory('bad')} text="bad"></Button>
 
-      <h2>Results</h2>
-      <p>Good votes: {good}</p>
-      <p>Good votes: {neutral}</p>
-      <p>Good votes: {bad}</p>
-      <br></br>
-      <p>Votes total: {calcTotal()}</p>
-      <p>Average score: {calcAverage()}</p>
-      <p>Amount positive: {calcPositivePercentage()}</p>
+      <Statistics votes={ {good, neutral, bad} }></Statistics>
+
     </div>
   )
 };
