2455aba3b49825b3fa4140b2fa31a195e49f306f
Tomi Lämsä <lamsatom@gmail.com>
2019-09-03 20:51:07 +0300

    2.11 puhelinluettelo step6 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
+import axios from 'axios';
 
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import PhoneBookEntries from './components/PhoneBookEntries';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
-  const [ newName, setNewName ] = useState('')
+  const [ persons, setPersons ] = useState([]);
+  const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ searchTerm, setSearchTerm ] = useState('');
 
-  // copy array to prevent possibly odd behaviour that could happen (?)
-  const [ filteredPersons, setFilteredPersons] = useState(persons.slice());
+  const [ filteredPersons, setFilteredPersons] = useState([]);
   console.log(filteredPersons);
+
+  useEffect(() => {
+    console.log('useEffect called');
+    
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('got response, res.data:', response.data);
+        setPersons(response.data);
+        setFilteredPersons(response.data);
+      })
+      .catch(err => {
+        console.error('error at fetching json:', err);
+      });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
