2d012671e691478f8d6fccbba06fa0462a8dd03f
Tomi Lämsä <lamsatom@gmail.com>
2019-09-09 10:42:50 +0300

    2.14 maiden tiedot step 3 done

--- part2/maiden_tiedot/src/components/Country.js
+++ part2/maiden_tiedot/src/components/Country.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Country = (({country}) => {
+
+  const [wind, setWind] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [weatherImgSrc, setWeatherImgSrc] = useState('');
+
     // just deal with showing the single country, let Content handle the logic
     const getLanguages = () => {
         return country.languages.map(language => {
@@ -10,6 +16,25 @@ const Country = (({country}) => {
         });
     }
 
+    const apiKey = ''; // enter your apixu API key here
+    if (apiKey === '') throw new Error('Enter API key before using this app');
+    const requestUrl = `http://api.apixu.com/v1/current.json?key=${apiKey}&q=`;
+
+    useEffect(() => {
+      axios
+        .get(`${requestUrl}${country.capital}`)
+        .then(res => {
+          console.log('got weather response:');
+          console.log(res.data);
+          setWind(res.data.current.wind_kph);
+          setTemperature(res.data.current.temp_c);
+          setWeatherImgSrc(res.data.current.condition.icon);
+        })
+        .catch(err => console.error(err));
+
+    }, []);
+
+
     return (
         <>
             <h1>{country.name}</h1>
@@ -18,6 +43,10 @@ const Country = (({country}) => {
             <h2>Languages</h2>
             <ul>{getLanguages()}</ul>
             <img src={country.flag} style={{maxWidth:'350px'}}></img>
+            <h2>Weather</h2>
+            <p>Current temperature: {temperature}C</p>
+            <img src={weatherImgSrc}></img>
+            <p>Wind:{wind}</p>
         </>
     )
 });
